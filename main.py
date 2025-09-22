2da3d04083208799e29e430779abda1cb0bad4a1
Pranav Anand24 <156640323+PranavAnand2005@users.noreply.github.com>
2025-09-22 23:57:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import json
 import pandas as pd
-import numpy as np
 
-data = pd.read_csv("data.csv")
-data.info()
+# with open("script.json","r") as file:
+#     data = json.load(file)
+#     for student in data:
+#         print("roll no",student["roll no"])
 
-import seaborn as sn
-from sklearn import preprocessing
-
-encode = perprocessing.oneHotEncoder()
-encode.fit(data[['region']]
-encode categories
-
-one_hot = encode.transform(data[['region']]).toarray()
-
-one_hot
+df = pd.read_json("script.json")
+print(df)
 
 
+
+dn = pd.json_normalize(df["Student"]) 
+print(dn)
+
+# df = pd.DataFrame('Student')
+# print(df.to_string(index=False))
