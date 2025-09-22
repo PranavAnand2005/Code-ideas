2f8f61174c32359992f14b0e548a9e01d991f23e
Pranav Anand24 <156640323+PranavAnand2005@users.noreply.github.com>
2025-09-22 22:38:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import pandas as pd
 import numpy as np
 
-data = pd.("data.csv")
+data = pd.read_csv("data.csv")
+data.info()
+
+import seaborn as sn
+from sklearn import preprocessing
+
+encode = perprocessing.oneHotEncoder()
+encode.fit(data[['region']]
+encode categories
+
+one_hot = encode.transform(data[['region']]).toarray()
+
+one_hot
+
+
